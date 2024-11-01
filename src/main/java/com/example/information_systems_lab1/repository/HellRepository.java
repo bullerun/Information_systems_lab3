da858ae527cd0ef632e30f70c9cc8e4ee8e6011c
bullerun <ryskalnikita2004@mail.ru>
2024-11-01 13:24:03 +0300

    Eclips + JPA + Spring

--- src/main/java/com/example/information_systems_lab1/repository/HellRepository.java
+++ src/main/java/com/example/information_systems_lab1/repository/HellRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HellRepository extends JpaRepository<Hell, Long> {
-    // Здесь вы можете добавить свои собственные методы, если это необходимо
-    // Например, метод для поиска пользователя по имени:
     Hell findByUsername(String username);
 }
