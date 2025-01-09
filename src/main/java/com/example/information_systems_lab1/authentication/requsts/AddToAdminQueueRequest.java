d795dc6fea5fc57052e1e39ca1c087484ff15dad
bullerun <ryskalnikita2004@mail.ru>
2025-01-09 11:40:02 +0300

    unused code has been deleted

--- src/main/java/com/example/information_systems_lab1/authentication/requsts/AddToAdminQueueRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.information_systems_lab1.authentication.requsts;
-
-import lombok.Data;
-
-@Data
-public class AddToAdminQueueRequest {
-    private Long id;
-}
--- src/main/java/com/example/information_systems_lab1/exception/OneStringException.java
+++
@@ -1,7 +0,0 @@
-package com.example.information_systems_lab1.exception;
-
-public class OneStringException extends Exception{
-    public OneStringException(String s){
-        super(s);
-    }
-}
